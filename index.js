057fe36bbb485a05340bf86aeaa8481031bedb70
Ryan-Wee-yh <ryanwyh05@gmail.com>
2023-02-03 16:25:40 +0800

    sign up API done, login+sign up pop ups working

---
+++ index.js
@@ -0,0 +1,71 @@
+//Login Form pop up
+function loginForm() {
+    document.getElementById("loginForm").style.display = "block";
+}
+  
+function closelogForm() {
+    document.getElementById("loginForm").style.display = "none";
+}
+
+//Sign Up Form pop up
+function signupForm() {
+    document.getElementById("signupForm").style.display = "block";
+}
+  
+function closesignForm() {
+    document.getElementById("signupForm").style.display = "none";
+}
+
+function hidesignin() {
+    var x = document.getElementById("");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+$(document).ready(function () {
+    //what kind of interface we want at the start 
+    const APIKEY = "63b7c054969f06502871ab6f";
+
+
+    $('#submit-btn').on("click", function (e){
+        //prevent default action of the button stops page from refreshing
+        e.preventDefault();
+
+        var username =  $('#username-field').val()
+        var password =  $('#password-field').val()
+        var level = 1
+        var points = 0
+    
+        var jsondata = {"username": username,
+                        "password": password,
+                        "level": level,
+                        "points": points};
+    
+        var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://interactivedev-2a8f.restdb.io/rest/account",
+        "method": "POST",
+        "headers": {
+            "content-type": "application/json",
+            "x-apikey": APIKEY,
+            "cache-control": "no-cache"
+        },
+        "processData": false,
+        "data": JSON.stringify(jsondata)
+        }
+    
+        $.ajax(settings).done(function (response) {
+        console.log(response);
+
+        
+        
+        });
+    })
+    
+    
+})
